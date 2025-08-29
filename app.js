e09bf96fefcccc00b206d6f6905b2d3951ba856f
surender18 <surenderempire68@gmail.com>
2025-08-29 22:59:34 +0530

    add like, edit functionality

---
+++ app.js
@@ -0,0 +1,112 @@
+const express= require('express')
+const app= express()
+const userModel=require('./model/user')
+const postModel=require('./model/post')
+const { default: mongoose } = require('mongoose')
+const cookieParser = require('cookie-parser')
+const bcrypt= require('bcrypt')
+const jwt = require('jsonwebtoken')
+const e = require('express')
+
+app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.get('/',(req,res)=>{
+    res.render('Index')
+})
+app.get('/login',(req,res)=>{
+    res.render('Login') 
+})
+app.get('/profile',isLoggedIn, async (req,res)=>{
+    let user= await userModel.findOne({email: req.user.email}).populate('posts')
+    res.render('Profile',{user})
+})
+app.get('/like/:id',isLoggedIn, async (req,res)=>{
+    let post= await postModel.findOne({_id: req.params.id}).populate('user')
+    if(post.likes.indexOf(req.user.userid)=== -1){
+    post.likes.push(req.user.userid)
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid),1)
+
+    }
+    
+    await post.save()
+    res.redirect('/profile')
+})
+app.get('/edit/:id',isLoggedIn, async (req,res)=>{
+    let post= await postModel.findOne({_id: req.params.id}).populate('user')
+   res.render('edit',{post})
+})
+app.post('/update/:id',isLoggedIn, async (req,res)=>{
+    let post= await postModel.findOneAndUpdate({_id: req.params.id},{content: req.body.content})
+   res.redirect('/profile')
+})
+
+app.post('/post',isLoggedIn, async (req,res)=>{
+    let user= await userModel.findOne({email: req.user.email})
+    let {content}=req.body;
+    let post=await postModel.create({
+        user:user._id,
+        content
+    })
+    user.posts.push(post._id)
+    await user.save();
+    res.redirect('/profile')
+})
+
+app.post('/register',async (req,res)=>{
+    let {name,email,username,password}=req.body;
+    let user=await userModel.findOne({email})
+    if(user) return res.status(500).redirect('/login')
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt, async(err,hash)=>{
+           let user= await userModel.create({   
+                username,
+                email,
+                password:hash
+            })
+           let token= jwt.sign({email:email,userid:user._id},'secret')
+           res.cookie('token',token)
+           res.send('user registered successfully')
+        })
+        
+    })
+
+})
+
+app.post('/login',async (req,res)=>{
+    let {email,password}=req.body;
+    let user=await userModel.findOne({email})
+    if(!user) return res.send('Something went wrong')
+
+    bcrypt.compare(password,user.password,(err,result)=>{
+        if(!result) return res.redirect('/login')
+        let token= jwt.sign({email:email,userid:user._id},'secret')
+        res.cookie('token',token)
+        res.status(200).redirect("/profile")
+    })
+   
+
+})
+app.get('/logout',(req,res)=>{
+    res.cookie('token','')
+    res.render('Login')
+})
+function isLoggedIn(req,res,next){
+    if(req.cookies.token ==='') return res.redirect('/login')
+        else{
+          let data=  jwt.verify(req.cookies.token,'secret')
+          req.user=data
+          next()
+    }
+    
+}
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
+
+
